e8dd2236b185d66681c2c6855a844f393e0e7c4c
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-07-22 22:25:24 +0300

    modified footer and product details

--- ecommerce/components/Footer.jsx
+++ ecommerce/components/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>
+    <div className='footer-container'>
+      <p>2024 JSM Headphone All rights reserved</p>
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
       
     </div>
   )
